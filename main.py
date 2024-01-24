47f45422d4c1bda36fcbd79fdac2d0498ea42837
Omar Medrano Sarabia <omedrano@bb.com.mx>
2024-01-24 15:35:57 -0600

    Metodo GET

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+movies = [
+    {"name": "The Godfather", "year": 1972},
+    {"name": "The Shawshank Redemption", "year": 1994},
+    {"name": "Schindler's List", "year": 1993},
+]
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return HTMLResponse("<h1>Go to /docs</h1>")
+
+
+@app.get("/movies", tags=["movies"])
+def get_movies():
+    return movies
